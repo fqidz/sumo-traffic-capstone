35f816e351a183d26e25b7779853fe8c178a2957
fqidz <meowthful127@gmail.com>
2024-03-05 20:00:37 +0300

    added traffic control py

---
+++ random.sh
@@ -0,0 +1,1 @@
+python3 "$SUMO_HOME/tools/randomTrips.py" -n "net.net.xml" --fringe-factor "max" --allow-fringe --vehicle-class "passenger" -e "3600" -o "trips.trips.xml" -r "routes.rou.xml"
---
+++ traffic-control.py
@@ -0,0 +1,15 @@
+import os
+import sys
+if 'SUMO_HOME' in os.environ:
+    sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
+import traci
+
+sumoBinary = "/usr/share/sumo/bin/sumo-gui"
+sumoCmd = [sumoBinary, "-c", "main.sumocfg"]
+
+traci.start(sumoCmd)
+for i in range(5000):
+    traci.simulationStep()
+    print(traci.vehicle.getIDCount())
+
+traci.close()
