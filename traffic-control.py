f45303ab1bc51e3932374d13bb6a60836d361290
fqidz <meowthful127@gmail.com>
2024-03-18 21:40:02 +0300

    initial reformat for preparation for ai learning

--- traffic-control.py
+++ traffic-control.py
@@ -1,36 +1,8 @@
-# TODO: replace with activityGen.py TODO: prompt if user wants to rerun randomTrips.py command argument stuff
-# TODO: also generate lane area detectors
-# TODO: move the todo functionalities to separate scripts
+# TODO: use queue length instead of count of cars
+# TODO: use waiting time for reward
+# TODO: check if cars are stuck in the junction
 import traci
-import random
 
-SUMOGUI_PATH = "/usr/share/sumo/bin/sumo-gui"
-SUMOCFG_PATH = "./sumo-things/main.sumocfg"
-
-sumo_cmd = [SUMOGUI_PATH, "-c", SUMOCFG_PATH]
-
-
-def count_cars_in_lanearea(lanearea_ids: tuple) -> dict:
-    """returns the amount of cars in each lanearea detector"""
-    # get the count of cars for each lane area detector
-    lanearea_count = {}
-    for i in lanearea_ids:
-        lanearea_count[i] = traci.lanearea.getLastStepVehicleNumber(i)
-
-    # sum the lanearea detector car counts into each of the directions
-    lanearea_count_sum = {}
-    for dir in ["east", "north", "south", "west"]:
-        direction_values = {key: value for key,
-                            value in lanearea_count.items() if key.startswith(dir)}
-        lanearea_count_sum[dir] = sum(direction_values.values())
-
-    return lanearea_count_sum
-
-
-def switchPhase(traffic_id: str, phase: int):
-    """switch traffic light phase"""
-    traci.trafficlight.setPhase(traffic_id, phase)
-    traci.trafficlight.setPhaseDuration(traffic_id, 100)
 
 # [car count east, car count north, car count south, car count west
 #  car avg speed east, car avg speed north, car avg speed south, car avg speed west
@@ -39,39 +11,73 @@ def switchPhase(traffic_id: str, phase: int):
 #
 # *format of: [rrGGyyrr] would be [0, 0, 1, 1, 0.5, 0.5, 0, 0]
 
+SUMOGUI_PATH = "/usr/share/sumo/bin/sumo-gui"
+SUMOCFG_PATH = "./sumo-things/main.sumocfg"
+
+sumo_cmd = [SUMOGUI_PATH, "-c", SUMOCFG_PATH]
+
+
+class TraciSim:
+    def __init__(self) -> None:
+        self.traffic_id = "0"
+        self.traffic_state = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.lanearea_ids = traci.lanearea.getIDList()
+
+    def countCarsInLanearea(self, lanearea_ids: tuple) -> dict:
+        """returns the amount of cars in each lanearea detector"""
+        # get the count of cars for each lane area detector
+        lanearea_count = {}
+        for i in lanearea_ids:
+            lanearea_count[i] = traci.lanearea.getLastStepVehicleNumber(i)
+
+        # sum the lanearea detector car counts into each of the directions
+        lanearea_count_sum = {}
+        for dir in ["east", "north", "south", "west"]:
+            direction_values = {key: value for key,
+                                value in lanearea_count.items() if key.startswith(dir)}
+            lanearea_count_sum[dir] = sum(direction_values.values())
+
+        return lanearea_count_sum
+
+    def setState(self, traffic_id: str, state_input: list[float]) -> None:
+        _state_output = []
+        # states are grouped together, so that each state represent a phase that matches with the lane
+        # so that eg. left turns and u-turns traffic light signals are grouped together
+        for i, phase in enumerate(state_input):
+            if (i % 2 == 0):
+                if (phase == 0.0):
+                    _state_output.append('rr')
+                elif (phase == 0.5):
+                    _state_output.append('yy')
+                elif (phase == 1.0):
+                    _state_output.append('GG')
+                else:
+                    raise Exception('State should be 0.0, 0.5, or 1.0')
+            else:
+                if (phase == 0.0):
+                    _state_output.append('rrrr')
+                elif (phase == 0.5):
+                    _state_output.append('yyyy')
+                elif (phase == 1.0):
+                    _state_output.append('GGGG')
+                else:
+                    raise Exception('State should be 0.0, 0.5, or 1.0')
+
+        _state_string = ''.join(_state_output)
+        print(_state_string)
+        traci.trafficlight.setRedYellowGreenState(traffic_id, _state_string)
+
+    def step(self):
+        self.countCarsInLanearea(self.lanearea_ids)
+        self.setState(self.traffic_id, self.traffic_state)
+        traci.simulationStep()
+
 
 traci.start(sumo_cmd)
-
-time_since_last_phase = 0
-traffic_phase_duration_green = 999999
-traffic_phase_duration_yellow = 5
-phase = 0
+sim = TraciSim()
 
 for i in range(5000):
-    lanearea_id_list = traci.lanearea.getIDList()
-    lanearea_car_count = count_cars_in_lanearea(lanearea_id_list)
-    print(lanearea_car_count)
 
-    if (time_since_last_phase == 0):
-        traffic_phase_duration_green = random.randint(20, 100)
-
-    print(f'traffic phase duration: {traffic_phase_duration_green}')
-    print(f'time since last phase: {time_since_last_phase}')
-
-    if (time_since_last_phase >= traffic_phase_duration_green):
-        # reset time since last phase
-        time_since_last_phase = 0
-        # choose random phase and switch to it
-        phase = random.choice([0, 2, 4, 6, 8, 10, 12, 14])
-        switchPhase("30896504", phase)
-
-    if (time_since_last_phase >= traffic_phase_duration_green - traffic_phase_duration_yellow):
-        # if close to next phase, change it to yellow light
-        switchPhase("30896504", phase + 1)
-
-    print(f'current phase: {traci.trafficlight.getPhase("30896504")}')
-
-    traci.simulationStep()
-    time_since_last_phase += 1
+    sim.step()
 
 traci.close()
