a428e2a01ae451f4f505f309b207548f597ee9e0
fqidz <meowthful127@gmail.com>
2024-03-27 00:09:33 +0300

    improved data processing

--- utils/stats-plot/stats-plot-csv-xml.py
+++ utils/stats-plot/stats-plot-csv-xml.py
@@ -1,47 +1,177 @@
 from os.path import isfile, join
 import os
+from typing import Union
+import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
 
-def extract_info(path_to_files: str, header_title: str, trim: int):
+class DataProcessing():
+    def __init__(self, headers: list[str], input_path: str, output_path: str, output_file_names: list[str], trim_direction: list[str], trim: list[int]) -> None:
+        """
+        Args:
+            data (dict): the data
+            trim (int): cut off point
+            direction (str): 'up' or 'down'
+        """
+        if len(headers) != len(output_file_names):
+            raise Exception("headers and output_file_names not same amount")
+        self.headers = headers
+        self.input_path = input_path
+        self.output_path = output_path
+        self.output_file_names = output_file_names
+        self.trim = trim
+        self.trim_direction = trim_direction
+        self.data_dicts = []
+        self.do_save = False
 
-    csv_files_path = [os.path.join(path_to_files, f) for f in os.listdir(
-        path_to_files) if isfile(join(path_to_files, f))]
+    def extract_info(self) -> list[dict]:
 
-    total_queue_length = {}
-    for file_path in csv_files_path:
+        csv_files_path = [os.path.join(self.input_path, f) for f in os.listdir(
+            self.input_path) if (isfile(join(self.input_path, f)) and f.endswith('.csv'))]
 
-        with open(file_path, newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            data = list(reader)
-            file_name = os.path.split(file_path)[1]
-            # extract the number from the file
-            file_number = int(file_name.removeprefix(
-                'traffic_sim_conn0_ep').removesuffix('.csv'))
-            # get the index of the header we're looking for
-            header_index = data[0].index(header_title)
+        _data_dicts = []
+        for header in self.headers:
+            data_dict = {}
+            for file_path in csv_files_path:
 
-            queue_length = 0
-            for row in data[1:]:
-                queue_length += int(row[header_index])
+                with open(file_path, newline='') as csvfile:
+                    reader = csv.reader(csvfile)
+                    data = list(reader)
+                    file_name = os.path.split(file_path)[1]
+                    # extract the number from the file
+                    file_number = int(file_name.removeprefix(
+                        'traffic_sim_conn0_ep').removesuffix('.csv'))
+                    # get the index of the header we're looking for
+                    header_index = data[0].index(header)
 
-            # trim big values
-            if queue_length <= trim:
-                total_queue_length[file_number] = queue_length
+                    if header == '0_queue_length':
+                        data_value = self.get_queue_length_sum(
+                            data, header_index)
+                    elif header == 'system_mean_speed':
+                        data_value = self.get_mean_speed(data, header_index)
+                    else:
+                        raise Exception("not implemented")
 
-    return dict(sorted(total_queue_length.items()))
+                    data_dict[file_number] = data_value
+
+            data_trimmed = self.trim_values(data_dict)
+            _data_dicts.append(dict(sorted(data_trimmed.items())))
+
+        return _data_dicts
+
+    def get_queue_length_sum(self, data: list, header_index: int) -> float:
+        queue_length = 0
+        for row in data[1:]:
+            queue_length += float(row[header_index])
+
+        return queue_length
+
+    def get_mean_speed(self, data: list, header_index: int) -> float:
+        average_speeds = []
+        for row in data[1:]:
+            average_speeds.append(float(row[header_index]))
+
+        total_average_speed = np.average(average_speeds)
+
+        return float(total_average_speed)
+
+    def trim_values(self, data: dict) -> dict:
+        # trim outlier values
+        for i, direction in enumerate(self.trim_direction):
+            for key, value in data.items():
+                if key > 30:
+                    if self.trim_direction[i] == 'up':
+                        if (value >= self.trim[i]):
+                            neighbors_mean = (
+                                data[key - 1] + data[key + 1]) / 2
+                            data[key] = neighbors_mean
+                            print(f"Episode {key} outlier (value of {value} >= {
+                                  self.trim[i]}): Adjusted to mean of neighbors ({neighbors_mean})")
+                    elif self.trim_direction[i] == 'down':
+                        if (value <= self.trim[i]):
+                            neighbors_mean = (
+                                data[key - 1] + data[key + 1]) / 2
+                            data[key] = neighbors_mean
+                            print(f"Episode {key} outlier (value of {value} <= {
+                                  self.trim[i]}): Adjusted to mean of neighbors ({neighbors_mean})")
+                    else:
+                        raise Exception(
+                            "trim_direction can only be 'up' or 'down'")
+        return data
+
+    def save_to_csv(self) -> None:
+        for i, data in enumerate(self.data_dicts):
+            with open(os.path.join(self.output_path, self.output_file_names[i]), 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+
+                writer.writerow(list(data.keys()))
+                writer.writerow(list(data.values()))
+            print(f"Saved {self.headers[i]} to {os.path.join(
+                self.output_path, self.output_file_names[i])}")
+
+    def load_csv(self) -> list[dict]:
+        loaded_data = []
+        for i, file_name in enumerate(self.output_file_names):
+            file = os.path.join(self.output_path, self.output_file_names[i])
+            if isfile(file):
+                with open(file, newline='') as csvfile:
+                    reader = csv.reader(csvfile)
+                    data = list(reader)
+                    data_dict = {}
+
+                    for i, header in enumerate(data[0]):
+                        data_dict[header] = float(data[1][i])
+
+                    loaded_data.append(dict(data_dict))
+
+        return loaded_data
+
+    def prompt_save(self):
+        repeat = True
+        while repeat:
+            save_prompt = input("Save data to csv? (y/N):").lower()
+            save_prompt = "".join(save_prompt.split())
+
+            if save_prompt == "y":
+                self.do_save = True
+                repeat = False
+            elif save_prompt == "n" or not save_prompt:
+                self.do_save = False
+                repeat = False
+            else:
+                print("Wrong Input")
+                print("")
+                repeat = True
+
+    def plot(self, dicts: list[dict]):
+        data_list: list[list] = []
+        for data in dicts:
+            data_list.append(list(data.values()))
+
+        fig, axs = plt.subplots(2)
+        axs[0].plot(data_list[0])
+        axs[1].plot(data_list[1], color='r')
+        plt.show()
+
+    def run(self):
+        self.prompt_save()
+        if self.do_save == True:
+            self.data_dicts = self.extract_info()
+            self.save_to_csv()
+        else:
+            self.data_dicts = self.load_csv()
+
+        self.plot(self.data_dicts)
 
 
-queue = extract_info(
+process = DataProcessing(
+    ['0_queue_length', 'system_mean_speed'],
     '/home/faidz-arante/Documents/sumo-traffic-capstone/output/dqn-stats/',
-    '0_queue_length',
-    320000,
+    '/home/faidz-arante/Documents/sumo-traffic-capstone/output/processed_data/',
+    ['queue_length.csv', 'mean_speeds.csv'],
+    ['up', 'down'],
+    [320000, 6]
 )
 
-queue_list = list(queue.values())
-
-plt.plot(queue_list)
-plt.ylabel('Total Queue Length')
-plt.xlabel('Episode')
-plt.show()
+process.run()
