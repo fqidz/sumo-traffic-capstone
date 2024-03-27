8df743a4b6ebe76b2fea3806e4c6035ca5c3f7b2
fqidz <meowthful127@gmail.com>
2024-03-27 23:25:21 +0300

    many changes

--- utils/dynamic-traffic-flow-generator/csv-xml-veh-hour.py
+++ utils/dynamic-traffic-flow-generator/csv-xml-veh-hour.py
@@ -1,42 +1,33 @@
 import csv
+import os
 
 
-def convert_csv_to_xml(csv_file, xml_file):
-    """
-    Converts a CSV file to XML elements.
+class VehicleFlowConverter():
+    def __init__(self, csv_files: list[str], vehicle_types: list[str], xml_file: str) -> None:
+        self.csv_files = csv_files
+        self.vehicle_types = vehicle_types
+        self.xml_file = xml_file
 
-    Args:
-      csv_file: Path to the CSV file.
-      xml_file: Path to the output XML file.
-    """
-    with open(csv_file, 'r') as csvfile, open(xml_file, 'w') as xmlfile:
-        reader = csv.reader(csvfile)
-        headers = next(reader)  # Get headers
+    def convert_csv_to_xml(self):
+        for i, csv_file in enumerate(self.csv_files):
+            vehicle_type = self.vehicle_types[i]
+            with open(csv_file, 'r') as csvfile, open(self.xml_file, 'w') as xmlfile:
+                reader = csv.reader(csvfile)
+                headers = next(reader)  # Get headers
 
-        # Write XML start tag
-        xmlfile.write('<flows>\n')
+                for j, row in enumerate(reader):
+                    begin, end = row[0], row[1]
 
-        for j, row in enumerate(reader):
-            begin, end = row[0], row[1]
+                    # Generate unique flow id and route
+                    # Skip first two columns
+                    for i, flow_id in enumerate(headers[2:]):
+                        route = flow_id
+                        unique_id = f"{flow_id}{j}"
 
-            # Generate unique flow id and route
-            for i, flow_id in enumerate(headers[2:]):  # Skip first two columns
-                route = flow_id
-                unique_id = f"{flow_id}{j}"
+                        vehs_per_hour = row[i + 2]
 
-                vehs_per_hour = row[i + 2]
+                        # Write flow element
+                        xmlfile.write(f'<flow id="{unique_id}" type={vehicle_type} begin="{begin}.00" route="{
+                                      route}" end="{end}.00" vehsPerHour="{vehs_per_hour}"/>\n')
 
-                # Write flow element
-                xmlfile.write(f'  <flow id="{unique_id}" begin="{begin}.00" route="{
-                              route}" end="{end}.00" vehsPerHour="{vehs_per_hour}"/>\n')
-
-        # Write XML end tag
-        xmlfile.write('</flows>')
-
-
-# Example usage
-csv_file = "traffic-veh-hour.csv"
-xml_file = "traffic_data.xml"
-convert_csv_to_xml(csv_file, xml_file)
-
-print(f"Converted CSV file '{csv_file}' to XML file '{xml_file}'.")
+                xmlfile.write('\n')
