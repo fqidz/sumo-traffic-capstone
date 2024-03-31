bcf5833fdd7f36c7249e358c4e85bad9c4a1abe9
fqidz <meowthful127@gmail.com>
2024-03-31 14:03:21 +0300

    edited yolo

--- object_detection/yolo.py
+++ object_detection/yolo.py
@@ -1,35 +1,21 @@
 from ultralytics import YOLO
-from ultralytics.solutions import distance_calculation
 import cv2
 
-model = YOLO("yolov8l.pt")
-names = model.model.names
-
+model = YOLO("yolov8m.pt")
+# results = model.track(source="2", show=True, conf=0.4,
+#                       save=False, tracker="bytetrack.yaml", classes=[2, 3, 5, 7])
 cap = cv2.VideoCapture(2)
-assert cap.isOpened(), "Error reading video file"
-w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH,
-             cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
-
-# # Video writer
-# video_writer = cv2.VideoWriter("distance_calculation.avi",
-#                                cv2.VideoWriter_fourcc(*'mp4v'),
-#                                fps,
-#                                (w, h))
-
-# Init distance-calculation obj
-dist_obj = distance_calculation.DistanceCalculation()
-dist_obj.set_args(names=names, view_img=True)
 
 while cap.isOpened():
-    success, im0 = cap.read()
-    if not success:
-        print("Video frame is empty or video processing has been successfully completed.")
-        break
+    success, frame = cap.read()
 
-    tracks = model.track(im0, persist=True, show=False)
-    im0 = dist_obj.start_process(im0, tracks)
-#    video_writer.write(im0)
-
-cap.release()
-# video_writer.release()
-cv2.destroyAllWindows()
+    if success:
+        results = model.track(
+            source=frame,
+            show=True,
+            persist=True,
+            conf=0.3,
+            save=False,
+            tracker="bytetrack.yaml",
+            classes=[2, 3, 5, 7]
+        )
